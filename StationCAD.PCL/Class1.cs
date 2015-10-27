4758466154a22bbfb3b078852002e601033f6b52
Sergio Ora <sergio.ora@graphitegear.com>
2015-10-27 08:30:05 -0400

    Adding new projects

---
+++ StationCAD.PCL/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StationCAD.PCL
+{
+    public class Class1
+    {
+    }
+}
---
+++ StationCAD.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StationCAD.Web.MvcApplication" Language="C#" %>
