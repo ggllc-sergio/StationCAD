9f8fa6ab993ad1da9e490ddf1c3c515539342d54
Sergio Ora <sergio.ora@graphitegear.com>
2015-12-24 12:24:21 -0500

    Adding Parse and Pushbots Support

--- StationCAD.Web/Global.asax.cs
+++ StationCAD.Web/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace StationCAD.Web
 {
@@ -13,6 +14,8 @@ namespace StationCAD.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
