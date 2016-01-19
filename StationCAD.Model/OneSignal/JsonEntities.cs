0e785ab5730127b1ed80657327526cfd26a7e017
Sergio Ora <sergio.ora@graphitegear.com>
2016-01-18 23:55:33 -0500

    implemented OneSignal API calls

---
+++ StationCAD.Model/OneSignal/JsonEntities.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace StationCAD.Model.OneSignal
+{
+
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class DeviceList : AbstractJsonEntity
+    {
+        [JsonProperty(PropertyName = "total_count")]
+        public int TotalCount { get; set; }
+        [JsonProperty()]
+        public int Offset { get; set; }
+        [JsonProperty()]
+        public int Limit { get; set; }
+        [JsonProperty(PropertyName = "players")]
+        public ICollection<Device> Devices { get; set; }
+    }
+
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class Device : AbstractJsonEntity
+    {
+        [JsonProperty()]
+        public string Identifier { get; set; }
+        [JsonProperty(PropertyName = "session_count")]
+        public int SessionCount { get; set; }
+        public string Laanguage { get; set; }
+        public string TimeZone { get; set; }
+        [JsonProperty(PropertyName = "game_version")]
+        public string GameVersion { get; set; }
+        [JsonProperty(PropertyName = "device_os")]
+        public string DeviceOS { get; set; }
+        [JsonProperty(PropertyName = "device_type")]
+        public string DeviceType { get; set; }
+        [JsonProperty(PropertyName = "device_model")]
+        public string DeviceModel { get; set; }
+        [JsonProperty(PropertyName = "ad_id")]
+        public string AdId { get; set; }
+        public ICollection<KeyValuePair<string, string>> Tags { get; set; }
+        [JsonProperty(PropertyName = "last_active")]
+        public string LastActive { get; set; }
+        public DateTime LastActiveDateTime
+        {
+            get
+            {
+                DateTime dt = new DateTime();
+                long unixTS = 0;
+                long.TryParse(LastActive, out unixTS);
+                if (unixTS > 0)
+                    dt = UnixTimeStampToDateTime(unixTS);
+                return dt;
+            }
+        }
+        [JsonProperty(PropertyName = "amount_spent")]
+        public string AmountSpent { get; set; }
+        [JsonProperty(PropertyName = "created_at")]
+        public string CreatedAt { get; set; }
+        public DateTime CreatedAtDateTime
+        {
+            get
+            {
+                DateTime dt = new DateTime();
+                long unixTS = 0;
+                long.TryParse(CreatedAt, out unixTS);
+                if (unixTS > 0)
+                    dt = UnixTimeStampToDateTime(unixTS);
+                return dt;
+            }
+        }
+        [JsonProperty(PropertyName = "invalid_identifier")]
+        public bool InvalidIdentifier { get; set; }
+        [JsonProperty(PropertyName = "badge_count")]
+        public int BadgeCount { get; set; }
+        
+    }
+
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class PushNotificationCreate : AbstractJsonEntity
+    {
+        [JsonProperty(PropertyName = "app_id")]
+        public string ApplicationId { get; set; }
+        [JsonProperty(PropertyName = "headings", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Dictionary<string, string> Headings { get; set; }
+        [JsonProperty(PropertyName = "contents")]
+        public Dictionary<string, string> Contents { get; set; }
+        [JsonProperty(PropertyName = "isIos", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool SendiOS { get; set; }
+        [JsonProperty(PropertyName = "isAndroid", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool SendAndroid { get; set; }
+        [JsonProperty(PropertyName = "isWP", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool SendWindowsPhone { get; set; }
+        [JsonProperty(PropertyName = "isAdm", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool SendAmazon { get; set; }
+        [JsonProperty(PropertyName = "isChrome", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool SendChromeExt { get; set; }
+        [JsonProperty(PropertyName = "isChromeWeb", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool SendChromeWeb { get; set; }
+        [JsonProperty(PropertyName = "isSafari", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool SendSafari { get; set; }
+        [JsonProperty(PropertyName = "isAnyWeb", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool SendAllWeb { get; set; }
+        [JsonProperty(PropertyName = "included_segments", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public ICollection<string> IncludeSegments { get; set; }
+        [JsonProperty(PropertyName = "excluded_segments", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public ICollection<string> ExcludeSegments { get; set; }
+        [JsonProperty(PropertyName = "include_player_ids", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public ICollection<string> IncludeDevices { get; set; }
+        [JsonProperty(PropertyName = "tags", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public ICollection<PushNotificationTag> IncludeTags { get; set; }
+        [JsonProperty(PropertyName = "ios_sound", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string iOSSound { get; set; }
+        [JsonProperty(PropertyName = "android_sound", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string AndroidSound { get; set; }
+        [JsonProperty(PropertyName = "adm_sound", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string AmazonSound { get; set; }
+        [JsonProperty(PropertyName = "wp_sound", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string WindowsPhoneSound { get; set; }
+        [JsonProperty(PropertyName = "wp_wns_sound", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Windows8_1PhoneSound { get; set; }
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public ICollection<KeyValuePair<string, string>> Data { get; set; }
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Url { get; set; }
+        [JsonProperty(PropertyName = "semd_after", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string SendAfter { get; set; }
+
+    }
+
+    [JsonObject()]
+    public class PushNotificationTag : AbstractJsonEntity
+    {
+        public string Key { get; set; }
+        public string Relation { get; set; }
+        public string Value { get; set; }
+    }
+
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class PushNotificationList : AbstractJsonEntity
+    {
+        [JsonProperty(PropertyName = "total_count")]
+        public int TotalCount { get; set; }
+        [JsonProperty()]
+        public int Offset { get; set; }
+        [JsonProperty()]
+        public int Limit { get; set; }
+        [JsonProperty()]
+        public ICollection<PushNotification> Notifications { get; set; }
+    }
+
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class PushNotification : AbstractJsonEntity
+    {
+
+        [JsonProperty()]
+        public string Id { get; set; }
+        [JsonProperty()]
+        public int Successful { get; set; }
+        [JsonProperty()]
+        public int Failed { get; set; }
+        [JsonProperty()]
+        public int Converted { get; set; }
+        [JsonProperty()]
+        public bool Cancelled { get; set; }
+        [JsonProperty(PropertyName = "queued_at")]
+        public long QueuedAt { get; set; }
+        [JsonProperty(PropertyName = "send_after")]
+        public long SendAfter { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, string> Data { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, string> Headings { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, string> Contents { get; set; }
+    }
+    
+    [JsonObject()]
+    public class ItemDictionary : AbstractJsonEntity
+    {
+        [JsonProperty()]
+        public Dictionary<string, string> Item { get; set; }
+    }
+
+
+    public abstract class AbstractJsonEntity
+    {
+        public static DateTime UnixTimeStampToDateTime(long unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+    }
+}
+
+
--- StationCAD.Tests/ProcessorTests.cs
+++ StationCAD.Tests/ProcessorTests.cs
@@ -25,6 +25,48 @@ namespace StationCAD.Tests
             GetPlayer().Wait();
         }
 
+
+        [TestMethod]
+        public void GetNotificationsTest()
+        {
+            GetNotifications().Wait();
+        }
+
+
+        [TestMethod]
+        public void GetNotificationTest()
+        {
+            GetNotification().Wait();
+        }
+
+
+        [TestMethod]
+        public void CreateNotificationJsonTest()
+        {
+            PushNotificationCreate push = new PushNotificationCreate();
+            push.ApplicationId = "950a66d5-c457-4c9e-9a8b-cca7ef8b8c68";
+            push.Contents = new Dictionary<string, string>(); 
+            push.Contents.Add("en", "Server API Test Mesage");
+            push.IncludeSegments = new List<string> { "All" };
+            push.SendiOS = false;
+
+            try {
+                string json = JsonUtil<PushNotificationCreate>.ToJson(push);
+                Console.WriteLine(json);
+            }
+            catch(Exception ex)
+            { Console.WriteLine(ex.ToString()); }
+        }
+
+
+        [TestMethod]
+        public void CreateNotificationTest()
+        {
+            CreateNotification().Wait();           
+        }
+
+        #region Protected Methods 
+
         protected async Task GetPlayer()
         {
             User player = new User { IdentificationNumber = "5605d53f-9d13-4865-a23c-26dfd92d5c4a" };
@@ -42,5 +84,41 @@ namespace StationCAD.Tests
             string json = JsonUtil<DeviceList>.ToJson(result);
             Console.WriteLine(json);
         }
+
+        protected async Task GetNotifications()
+        {
+            PushNotifications pushNotifier = new PushNotifications();
+            PushNotificationList result = await pushNotifier.GetNotifications();
+            string json = JsonUtil<PushNotificationList>.ToJson(result);
+            Console.WriteLine(json);
+        }
+
+        protected async Task GetNotification()
+        {
+            PushNotifications pushNotifier = new PushNotifications();
+            PushNotification result = await pushNotifier.GetNotification("bee2a35c-9f5d-425d-b4ff-6196e033efc0");
+            string json = JsonUtil<PushNotification>.ToJson(result);
+            Console.WriteLine(json);
+        }
+
+        protected async Task CreateNotification()
+        {
+            PushNotificationCreate push = new PushNotificationCreate();
+            push.Contents = new Dictionary<string, string>();
+            push.Contents.Add("en", "Server API Test Mesage");
+            push.IncludeSegments = new List<string> { "All" };
+            push.SendiOS = false;
+
+            try
+            {
+                PushNotifications pushNotifier = new PushNotifications();
+                string result = await pushNotifier.CreatNotification(push);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.ToString()); }
+        }
+        #endregion
+
     }
 }
