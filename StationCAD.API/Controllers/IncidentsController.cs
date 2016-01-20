0aa2da15bdff9cd49bc96736d7a2e568e67d358d
Sergio Ora <sergio.ora@graphitegear.com>
2016-01-19 22:32:42 -0500

    Adding Incidents Email API Controller

---
+++ StationCAD.API/Controllers/IncidentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StationCAD.API.Controllers
+{
+    public class IncidentsController : ApiController
+    {
+
+        [Route("api/incidents/email")]
+        public IHttpActionResult ProcessIncidentEmail([FromBody] string json)
+        {
+
+            return Ok("Email processed successfully");
+        }
+    }
+}
