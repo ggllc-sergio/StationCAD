b9cbb17b5e3fabbf0608ea70ca44f0de02be6d25
Sergio Ora <sergio.ora@graphitegear.com>
2016-01-22 14:43:05 -0500

    Notificaion work

--- StationCAD.Processor/NotificationManager.cs
+++ StationCAD.Processor/NotificationManager.cs
@@ -13,10 +13,24 @@ namespace StationCAD.Processor
     public class NotificationManager
     {
 
-        public void NotifyUsers()
+        public void NotifyUsers(List<OrganizationUserNotifcation> users)
         {
 
+            // First group the notifications by notificaion Type
+            List<NotificationGroup> groups = users
+                .GroupBy(g => g.NotifcationType)
+                .Select(x => new NotificationGroup { Type = x.Key, Users = x.ToList<OrganizationUserNotifcation>() })
+                .ToList<NotificationGroup>();
+            // Use TPL to process each type of notification
+            if (groups.Count> 0)
+            {
+                ParallelOptions tplOptions = new ParallelOptions();
+                tplOptions.MaxDegreeOfParallelism = ParallelismFactor;
+                Parallel.ForEach<NotificationGroup>(groups, x => ProcessNotificationGroup(x));
+            }
         }
+
+        #region Type based notification creation 
 
         protected SMSNotification CreateSMSNotification(OrganizationUserNotifcation notification)
         {
@@ -32,5 +46,56 @@ namespace StationCAD.Processor
         {
             return new PushNotification();
         }
+
+        #endregion
+
+        #region Methods for sending notificaions 
+
+        #endregion
+
+        #region TPL task for notification processing 
+
+        private void ProcessNotificationGroup(NotificationGroup group)
+        {
+            switch(group.Type)
+            {
+                case OrganizationUserNotifcationType.Email:
+
+                    break;
+                case OrganizationUserNotifcationType.TextMessage:
+
+                    break;
+
+                default:
+
+
+                    break;
+            }
+        }
+        #endregion
+
+
+        protected int ParallelismFactor
+        {
+            get
+            {
+                int dop = 1;
+                int procCnt = Environment.ProcessorCount;
+                // if the available procs are 4 or more, use half.
+                // otherwise just use 1.
+                if (procCnt > 3)
+                { dop = procCnt / 2; }
+                else
+                { dop = 1; }
+
+                return dop;
+            }
+        }
+    }
+
+    public class NotificationGroup
+    {
+        public OrganizationUserNotifcationType Type { get; set; }
+        public List<OrganizationUserNotifcation> Users { get; set; }
     }
 }
