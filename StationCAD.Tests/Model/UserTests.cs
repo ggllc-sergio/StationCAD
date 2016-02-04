7ee008c173dd2d405a1a14763576f92f4e4449c5
Sergio Ora <sergio.ora@graphitegear.com>
2016-02-04 08:33:51 -0500

    Adding Migrations. UserTest Test config

--- StationCAD.Tests/Model/UserTests.cs
+++ StationCAD.Tests/Model/UserTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Linq;
 using StationCAD.Model;
 using StationCAD.Model.DataContexts;
 
@@ -28,6 +28,21 @@ namespace StationCAD.Tests.Model
             usr.LastName = string.Format("LastName_{0}", DateTime.Now.Ticks);
             usr.IdentificationNumber = DateTime.Now.Ticks.ToString();
             usr.UserName = string.Format("{0}.{1}", usr.FirstName, usr.LastName);
+            
+            using (var db = new StationCADDb())
+            {
+                try
+                {
+                    db.Users.Add(usr);
+                    db.SaveChanges();
+
+                    Assert.IsTrue(usr.Id > 0);
+
+                    User afterUser = db.Users.Where(x => x.IdentificationNumber == usr.IdentificationNumber).FirstOrDefault();
+                }
+                catch(Exception ex)
+                { Console.WriteLine(ex.Message); }
+            }
         }
     }
 }
